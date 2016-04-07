ee84b10a7d40701c52ed39587330cd8498b47d3c
emrehan <emrehan.tuzun@gmail.com>
2016-04-07 23:21:57 +0300

    Use mongoengine with remote mlab server

--- server.py
+++ server.py
@@ -1,14 +1,25 @@
 from datetime import datetime
+from bson import Binary, Code
+from bson.json_util import dumps
 from flask import Flask, request, jsonify
-from flask.ext.pymongo import PyMongo
+from flask.ext.mongoengine import MongoEngine
+from mongoengine import *
 
-import bson
 import json
 import os
 import requests 
+import uuid
+
+class Travel(DynamicDocument):
+    travel_id = StringField()
 
 app = Flask(__name__)
-mongo = PyMongo(app)
+app.config['MONGODB_SETTINGS'] = {
+    'db': 'cogeq',
+    'host': 'mongodb://u:p@ds013270.mlab.com:13270/cogeq'
+}
+db = MongoEngine(app)    
+
 config = json.load(open('config.json'))
 
 @app.route("/", methods=['GET'])
@@ -18,15 +29,11 @@ def hello():
 @app.route("/db", methods=['GET', 'POST', 'DELETE'])
 def dummy_db():
     if request.method == 'GET':
-        return bson.dumps( {'travels': list( mongo.db.travels.find() )} )
+        return dumps( {'travels': Travel.objects()} )
     if request.method == 'POST':
-        travel = {'city': 'Istanbul, Turkey'}
-        travel_id = mongo.db.travels.insert_one(travel).inserted_id
-        print (travel_id)
-        return bson.dumps( {'id': travel_id} )
-    if request.method == 'DELETE':
-        r = mongo.db.travels.delete_many({})
-        return bson.dumps( {'deleted_count': r.deleted_count } )
+        travel = Travel(travel_id = str(uuid.uuid1()))
+        travel.save()
+        return dumps( {'travel': travel} )
 
 @app.route("/cities", methods=['GET'])
 def search_cities():
@@ -36,12 +43,12 @@ def search_cities():
         r = requests.get('https://maps.googleapis.com/maps/api/place/autocomplete/json?types=(cities)&key=' + config['keys']['google_places'] + '&input=' + query )        
         if (r.status_code == 200):     
             cities = list( map( lambda x: x['description'], r.json()['predictions'] ) )
-            return bson.dumps( {'cities': cities}, ensure_ascii=False )
+            return dumps( {'cities': cities}, ensure_ascii=False )
         else:
-            return bson.dumps( {'Error': 'Error while getting Google Place API response'}, ensure_ascii=False )
+            return dumps( {'Error': 'Error while getting Google Place API response'}, ensure_ascii=False )
     else:
         default_cities = ['Istanbul, Turkey', 'London, United Kingdom', 'Izmir, Turkey', 'Singapore, Singapore', 'NYC, United States']
-        return bson.dumps( {'cities': default_cities}, ensure_ascii=False)
+        return dumps( {'cities': default_cities}, ensure_ascii=False)
         
 @app.route("/travels", methods=['POST'])
 def create_travel():
@@ -57,7 +64,7 @@ def create_travel():
             activity2 = {"id": "1234", "name": "Kofi", "type": "visit", "picture_url": "http://img.bleacherreport.net/img/slides/photos/002/990/036/kofi-kingston1_crop_north.jpg?w=630&h=420&q=75", "description": "Kofi is a nice place", "place": "", "directions": "", "from": ffrom.strftime(timeFormat), "to": to.strftime(timeFormat)}
             activities = [activity1, activity2];
             travel = { 'city': city, 'from': ffrom.strftime(timeFormat), 'to': to.strftime(timeFormat), 'activities': activities }
-            travel_id = mongo.db.travels.insert_one(travel).inserted_id
+            travel_id = db.travels.insert_one(travel).inserted_id
             return dumps( { 'travel_id': travel_id, 'from': ffrom.strftime(timeFormat), 'to': to.strftime(timeFormat), 'activities': activities } )
     except:
         return dumps({'Error': 'Error occured'})
@@ -65,14 +72,14 @@ def create_travel():
 @app.route("/travels/<travel_id>", methods=['GET', 'PUT'])
 def travel(travel_id):
     if request.method == 'GET':
-        return bson.dumps( {'travels': list( mongo.db.travels.find({"_id": bson.ObjectId(oid=str(travel_id))}) )} )
+        return dumps( {'travels': list( db.travels.find({"_id": bson.ObjectId(oid=str(travel_id))}) )} )
     if request.method == 'PUT':
         timeFormat = "%Y-%m-%dT%H:%M:%S";
         city = request.args.get('city')
         ffrom = datetime.strptime(request.args.get('from'), timeFormat)
         to = datetime.strptime(request.args.get('to'), timeFormat)
-        mongo.db.travels.update_one({"_id": bson.ObjectId(oid=str(travel_id))}, {"$set": {"city": city, "from": ffrom.strftime(timeFormat), "to": to.strftime(timeFormat) }})
-        return bson.dumps( { 'travel_id': travel_id, 'from': ffrom.strftime(timeFormat), 'to': to.strftime(timeFormat) } )
+        db.travels.update_one({"_id": bson.ObjectId(oid=str(travel_id))}, {"$set": {"city": city, "from": ffrom.strftime(timeFormat), "to": to.strftime(timeFormat) }})
+        return dumps( { 'travel_id': travel_id, 'from': ffrom.strftime(timeFormat), 'to': to.strftime(timeFormat) } )
 
 if __name__ == "__main__":
     app.debug = True 
