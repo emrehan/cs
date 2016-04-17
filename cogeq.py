a05670918e1e0988e7c2a31e74a7432631c2d65d
K. Bartu Komurcu <kbartukomurcu@gmail.com>
2016-04-17 14:58:08 +0300

    travels algorithm start

--- cogeq.py
+++ cogeq.py
@@ -71,10 +71,9 @@ def create_travel():
         access_token = request.args.get('access_token')
         ffrom = datetime.strptime(request.args.get('from'), timeFormat)
         to = datetime.strptime(request.args.get('to'), timeFormat)
-        if (not city or not access_tokent or not ffrom or not to):
+        if (not city or not access_token or not ffrom or not to):
             return dumps( {'Error': 'city, from and to must be provided.'} )
         else:
-            return dumps( {"access_token": access_token})
             r = requests.get("https://api.foursquare.com/v2/users/self/checkins?oauth_token=" + access_token + "&limit=250&offset=0&v=20160417")
             activity1 = {"id": "123", "name": "Kizilay", "type": "visit", "picture_url": "https://pbs.twimg.com/profile_images/666942007/kizilay_logo545px.png", "description": "Kizilay is a nice place", "place": "", "directions": "", "from": ffrom.strftime(timeFormat), "to": to.strftime(timeFormat)}
             activity2 = {"id": "1234", "name": "Kofi", "type": "visit", "picture_url": "http://img.bleacherreport.net/img/slides/photos/002/990/036/kofi-kingston1_crop_north.jpg?w=630&h=420&q=75", "description": "Kofi is a nice place", "place": "", "directions": "", "from": ffrom.strftime(timeFormat), "to": to.strftime(timeFormat)}
