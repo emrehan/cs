c249b5d0ae14440b1fb911f7bfd77cc07beba013
K. Bartu Komurcu <kbartukomurcu@gmail.com>
2016-05-01 15:17:56 +0300

    travels changed#4

--- cogeq.py
+++ cogeq.py
@@ -195,7 +195,7 @@ def create_travel():
                              "description": activity.description, "from": activity.ffrom,
                              "to": activity.to}
                 activitesArr.append(activity)
-            return ("asd")
+            return dumps({"activities": activitesArr})
             return dumps({'travel_id': 3, 'from': ffrom.strftime(timeFormat), 'to': to.strftime(timeFormat), 'activities': activitesArr})
     except:
         return dumps({'Error': 'Error occured'})
